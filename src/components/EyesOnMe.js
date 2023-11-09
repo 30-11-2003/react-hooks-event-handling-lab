2535d35d2e294df570a920637615d5b3e8d29111
Shalom Orangi <shalom.orangi@student.moringaschool.com>
2023-11-09 18:27:00 +0300

    Updated

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,26 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe(){
+ 
+    function handleFocus (e){
+        e.preventDefault()
+        console.log ("Good!")
+      
+    }
+  
+    function handleBlur(e){
+        e.preventDefault()
+        console.log ("Hey! Eyes on me!")
+      
+    }
+    return(
+        <div
+         onFocus={handleFocus}
+        onBlur={handleBlur}>
+        <button>Eyes on me</button>
+        </div>
+    )
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,30 @@
 // Code Keypad Component Here
+import React from "react";
+
 
 function Keypad (){
+
+function handlePassword (e) {
+e.preventDefault()
+console.log("Entering Password...")
+}
+
+function handleSubmit (e) {
+    e.preventDefault()
+
+}
+
     return (
-        <div></div>
+        <div>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="password">Password</label>
+            <input type="password" 
+            id="password" 
+            onChange={handlePassword} >
+            </input>
+            <button>Enter</button>
+            </form>
+        </div>
     )
 }
 
